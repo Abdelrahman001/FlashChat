8704c08aa2d39ce7dcf47ed9b2465216b29841ea
Abdelrahman001 <abdelrahman.hassan001@gmail.com>
2020-03-31 14:31:45 +0200

    Upload Project Files.

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Abdelrahman on 3/15/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+import Foundation
+import Firebase
+
+struct Message {
+    let sender: String
+    let body: String
+}
